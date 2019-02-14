5e2d2233ae3fcd15584888dca3ae1a2f6fbebb28
Peter Blasko <blasko.peter.hu@gmail.com>
2019-02-14 20:56:59 +0100

    create ShowRepository

---
+++ src/main/java/com/codecool/jpa/Repository/ShowRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.jpa.Repository;
+
+import com.codecool.jpa.Model.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowRepository extends JpaRepository<Show, Integer> {
+}
