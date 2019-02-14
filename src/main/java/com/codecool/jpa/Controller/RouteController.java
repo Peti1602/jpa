57bef3b5cde0864625a9b27dfcd5cc7f38bef7c0
Peter Blasko <blasko.peter.hu@gmail.com>
2019-02-14 21:04:12 +0100

    add title and shows attributes to routeController index function

--- src/main/java/com/codecool/jpa/Controller/RouteController.java
+++ src/main/java/com/codecool/jpa/Controller/RouteController.java
@@ -1,13 +1,21 @@
 package com.codecool.jpa.Controller;
 
+import com.codecool.jpa.Service.ShowService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RouteController {
 
+    @Autowired
+    private ShowService showService;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("title", "All Shows");
+        model.addAttribute("shows", showService.getAllShows());
         return "index";
     }
 
